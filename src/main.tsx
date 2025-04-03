8953b8b270bd23caaf80123f6fa99eaabb7b2d4c
DanielFigu <163463666+DanielFigu@users.noreply.github.com>
2025-04-02 22:28:58 -0600

    Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Categorias from "./Categorias";
-import Cliente from "./Cliente";
-import Productos from "./Productos";
-import Venta from "./Venta";
+import Categorias from "./components/Categorias/Categorias";
+import Cliente from "./components/Cliente/Cliente";
+import Productos from "./components/Productos/Productos";
+import Venta from "./components/Venta/Venta";
 import Menu from "./components/Menu/Menu";
+import AppComponent from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<AppComponent />} />
         <Route path="/categorias" element={<Categorias />} />
         <Route path="/cliente" element={<Cliente />} />
         <Route path="/productos" element={<Productos />} />
