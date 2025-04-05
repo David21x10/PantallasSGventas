fae53f0c25ef884f226098f1cb9e65372292516a
Gisel <giselvela.2002@gmail.com>
2025-04-04 21:27:55 -0600

    Cambios de diseño y remover funcionalidad

--- src/components/Cliente/Cliente.tsx
+++ src/components/Cliente/Cliente.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useState } from "react";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaEdit, FaTrash } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import "./Cliente.css"
 import { validarEmail, validarIdNumerico, validarNombre, validarTelefono } from "../../utilidades/validaciones";
@@ -18,11 +18,20 @@ interface IErrores {
   telefono: string | null
 }
 
+const ejemploCliente: ICliente[] = [
+  {
+    id: '1',
+    nombre: 'Test',
+    email: 'test@test.com',
+    telefono: "12345678"
+  }
+]
+
 const Cliente: React.FC = () => {
   const navigate = useNavigate();
   const [editando, setEditando] = useState(false)
 
-  const [clientes, setClientes] = useState<ICliente[]>([])
+  const [clientes, setClientes] = useState<ICliente[]>(ejemploCliente)
   const [cliente, setCliente] = useState<ICliente>({
     id: "",
     nombre: "",
@@ -48,23 +57,16 @@ const Cliente: React.FC = () => {
     if (errorNombre || errorId || errorEmail || errorTelefono) return
 
     if (editando) {
-      const clientesEditados = clientes.map(clienteEditar => clienteEditar.id === cliente.id ? {
-        ...clienteEditar,
-        nombre: cliente.nombre,
-        email: cliente.email,
-        telefono: cliente.telefono
-      }
-        : clienteEditar)
-      setClientes(clientesEditados)
+      // TODO: Agregar editar cliente y actualizar lista
       resetearCliente()
       setEditando(false)
       return
     }
-    setClientes([...clientes, cliente])
+    // TODO: Agregar llamada a agregar cliente y actualizar lista
     resetearCliente()
   }
   const handleEditar = (id: string) => {
-    const editarCliente = clientes.find(cliente => cliente.id === id)
+    const editarCliente = obtenerClienteAEditar(id);
 
     if (!editarCliente)
       return
@@ -73,7 +75,7 @@ const Cliente: React.FC = () => {
     setEditando(true)
   }
   const handleEliminar = (id: string) => {
-    setClientes(clientes.filter(cliente => cliente.id !== id))
+    // TODO: Agregar eliminar cliente en base al ID
   }
 
   const handleCancelarEdicion = () => {
@@ -96,117 +98,123 @@ const Cliente: React.FC = () => {
     })
   }
 
+  const obtenerClienteAEditar = (id: string): ICliente | undefined => {
+    // TODO: Agregar obtener cliente a editar en base al ID
+    return undefined;
+  }
+
   return (
-    <div>
-      <header className="cliente-header">
-        <div className="volver-menu-button" onClick={() => navigate("/")}>
-          <FaArrowLeft /> Volver al Menú
-        </div>
-        <h1>Clientes</h1>
-      </header>
-      <div className="cliente-container">
-        <div className="input-group">
-          <label>ID Cliente:</label>
-          <input
-            disabled={editando}
-            type="number"
-            value={cliente.id}
-            onChange={handleInputChange}
-            placeholder="Ingrese el ID"
-            min="1"
-            name="id"
-          />
+    <div className="cliente-container">
+      <div className="menu-button-container">
+        <button className="btn-back" onClick={() => navigate("/")}>Regresar al Menú</button>
+      </div>
 
-          {errores?.id && <p className="input-error">{errores.id}</p>}
-        </div>
+      <div className="cliente-content">
+        <h2 className="cliente-title">Clientes</h2>
+        <div className="cliente-formulario">
+          <div className="input-group">
+            <label>ID Cliente:</label>
+            <input
+              disabled={editando}
+              type="number"
+              value={cliente.id}
+              onChange={handleInputChange}
+              placeholder="Ingrese el ID"
+              min="1"
+              name="id"
+            />
 
-        <div className="input-group">
-          <label>Nombre:</label>
-          <input
-            type="text"
-            value={cliente.nombre}
-            onChange={handleInputChange}
-            placeholder="Ingrese el nombre"
-            name="nombre"
-          />
+            {errores?.id && <p className="input-error">{errores.id}</p>}
+          </div>
 
-          {errores?.nombre && <p className="input-error">{errores.nombre}</p>}
-        </div>
+          <div className="input-group">
+            <label>Nombre:</label>
+            <input
+              type="text"
+              value={cliente.nombre}
+              onChange={handleInputChange}
+              placeholder="Ingrese el nombre"
+              name="nombre"
+            />
 
-        <div className="input-group">
-          <label>Email:</label>
-          <input
-            type="email"
-            value={cliente.email}
-            onChange={handleInputChange}
-            placeholder="Ingrese el email"
-            name="email"
-          />
+            {errores?.nombre && <p className="input-error">{errores.nombre}</p>}
+          </div>
 
-          {errores?.email && <p className="input-error">{errores.email}</p>}
-        </div>
+          <div className="input-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              value={cliente.email}
+              onChange={handleInputChange}
+              placeholder="Ingrese el email"
+              name="email"
+            />
 
-        <div className="input-group">
-          <label>Telefono:</label>
-          <input
-            type="tel"
-            value={cliente.telefono}
-            onChange={handleInputChange}
-            placeholder="Ingrese el teléfono"
-            name="telefono"
-          />
+            {errores?.email && <p className="input-error">{errores.email}</p>}
+          </div>
 
-          {errores?.telefono && <p className="input-error">{errores.telefono}</p>}
-        </div>
+          <div className="input-group">
+            <label>Telefono:</label>
+            <input
+              type="tel"
+              value={cliente.telefono}
+              onChange={handleInputChange}
+              placeholder="Ingrese el teléfono"
+              name="telefono"
+            />
 
-        <div className="input-group">
-          <button onClick={handleAgregar}>
-            {editando ? "Actualizar" : "Ingresar"}
-          </button>
-          {
-            editando && <button onClick={handleCancelarEdicion} className="btn-cancelar">
-              Cancelar
+            {errores?.telefono && <p className="input-error">{errores.telefono}</p>}
+          </div>
+
+          <div className="input-group">
+            <button onClick={handleAgregar}>
+              {editando ? "Actualizar" : "Ingresar"}
             </button>
-          }
+            {
+              editando && <button onClick={handleCancelarEdicion} className="btn-cancelar">
+                Cancelar
+              </button>
+            }
 
+          </div>
+        </div>
+
+        <div className="tabla-container">
+
+          <table className="tabla-cliente">
+            <thead>
+              <tr>
+                <th>ID Cliente</th>
+                <th>Nombre</th>
+                <th>Email</th>
+                <th>Telefono</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {clientes.map((cliente) => (
+                <tr key={cliente.id}>
+                  <td>{cliente.id}</td>
+                  <td>{cliente.nombre}</td>
+                  <td>{cliente.email}</td>
+                  <td>{cliente.telefono}</td>
+                  <td>
+                    <div className="acciones-container">
+                      <button className="btn-editar" onClick={() => handleEditar(cliente.id)}>
+                        <FaEdit />
+                      </button>
+                      <button className="btn-eliminar" onClick={() => handleEliminar(cliente.id)}>
+                        <FaTrash />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
 
-      <table className="tabla-cliente">
-        <thead>
-          <tr>
-            <th>ID Cliente</th>
-            <th>Nombre</th>
-            <th>Email</th>
-            <th>Telefono</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {clientes.map((cliente) => (
-            <tr key={cliente.id}>
-              <td>{cliente.id}</td>
-              <td>{cliente.nombre}</td>
-              <td>{cliente.email}</td>
-              <td>{cliente.telefono}</td>
-              <td>
-                <button
-                  className="btn-editar"
-                  onClick={() => handleEditar(cliente.id)}
-                >
-                  Editar
-                </button>
-                <button
-                  className="btn-eliminar"
-                  onClick={() => handleEliminar(cliente.id)}
-                >
-                  Eliminar
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
 
       {/* FOOTER */}
       <footer className="cliente-footer">
